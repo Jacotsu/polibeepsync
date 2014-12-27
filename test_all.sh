b9f5ed6fbad3a4b6e7ab45cdf18ddc905404e51e
Davide Olianas <ubuntupk@gmail.com>
2014-12-27 15:35:14 +0100

    Test script: test only useful files

--- test_all.sh
+++ test_all.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 py.test --cov polibeepsync polibeepsync
-pyflakes polibeepsync/*.py
+pyflakes polibeepsync/qtgui.py
+pyflakes polibeepsync/common.py
 pyflakes polibeepsync/tests/*.py
-flake8 polibeepsync/*.py
+flake8 polibeepsync/qtgui.py
+flake8 polibeepsync/common.py
 flake8 polibeepsync/tests/*.py
 coverage html
 xdg-open htmlcov/index.html
