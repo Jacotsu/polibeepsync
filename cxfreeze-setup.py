9b4b263abe1c720d58773af9f34cf3d25c9a01e1
Davide Olianas <ubuntupk@gmail.com>
2015-01-30 22:30:02 +0100

    It's a GUI, not a cli program

--- cxfreeze-setup.py
+++ cxfreeze-setup.py
@@ -1,18 +1,23 @@
 from cx_Freeze import setup, Executable
 import requests.certs
 import os
+import sys
 
 build_exe_options = {"packages": ['requests', 'polibeepsync'],
                      "include_files":[(requests.certs.where(),'cacert.pem')],
                      }
 
+base=None
+if sys.platform == 'win32':
+	base = 'Win32GUI'
+
 executables = [
-    Executable(os.path.join('polibeepsync', 'qtgui.py'))
+    Executable(os.path.join('polibeepsync', 'qtgui.py'), base=base)
 ]
 
 setup(
-    name="prova",
-    version="0.1.1",
+    name="poliBeePsync",
+    version="0.3.1a",
     options = {'build_exe': build_exe_options},
       executables=executables, requires=['requests', 'PySide']
       )
