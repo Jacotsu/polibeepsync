fe090b789dbbb0703a283ffd31eb14cdbaac18ca
Jacotsu <Jacotsu@users.noreply.github.com>
2018-08-09 22:42:55 +0200

    Started plugin system implementation and setup.py update

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 import re
 
@@ -7,12 +7,14 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
+
 def read(*names, **kwargs):
     with open(
         os.path.join(os.path.dirname(__file__), *names),
         encoding=kwargs.get("encoding", "utf8")
     ) as fp:
         return fp.read()
+
 
 def find_version(*file_paths):
     version_file = read(*file_paths)
@@ -27,7 +29,8 @@ classifiers = [
     "Development Status :: 4 - Beta",
     "Environment :: X11 Applications :: Qt",
     "Intended Audience :: Education",
-    "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    """License :: OSI Approved :: GNU General Public License v3 or later
+    (GPLv3+)""",
     "Natural Language :: English",
     "Operating System :: OS Independent",
     "Programming Language :: Python :: 3.6"
@@ -35,12 +38,16 @@ classifiers = [
 
 setup(
     name='poliBeePsync',
-    version = find_version("polibeepsync/__init__.py"),
-    url = "http://www.davideolianas.com/polibeepsync",
-    author= "Davide Olianas",
-    author_email= "ubuntupk@gmail.com",
-    license = 'GPLv3+',
-    install_requires = [
+    version=find_version("polibeepsync/__init__.py"),
+    url="http://www.davideolianas.com/polibeepsync",
+    project_urls={
+        "Documentation": "https://jacotsu.github.io/polibeepsync",
+        "Source Code": "https://github.com/Jacotsu/polibeepsync",
+    },
+    author="Davide Olianas, Di Campli Raffaele",
+    author_email="ubuntupk@gmail.com",
+    license='GPLv3+',
+    install_requires=[
         "requests",
         "appdirs",
         "PySide2",
@@ -48,19 +55,20 @@ setup(
         "pyparsing",
         "signalslot"
     ],
-    packages = ['polibeepsync'],
-    description = "Sync files from https://beep.metid.polimi.it "
-                 "(for students of Politecnico di Milano)",
-    long_description = long_description,
-    classifiers = classifiers,
-    dependency_links = ['http://github.com/davethecipo/signalslot/tarball'
-                        '/py34#egg=signalslot-0.10.0'],
-    entry_points = {
-    'console_scripts': [
-        'polibeepsync=polibeepsync.qtgui:main',
-    ],
-    'gui_scripts': [
-        'polibeepsync-gui=polibeepsync.qtgui:main'
-    ]
-}
+    packages=find_packages(),
+    description="Sync files from https://beep.metid.polimi.it "
+                "(for students of Politecnico di Milano)",
+    long_description=long_description,
+    classifiers=classifiers,
+    dependency_links=['http://github.com/davethecipo/signalslot/tarball'
+                      '/py34#egg=signalslot-0.10.0'],
+    entry_points={
+        'console_scripts': [
+            'polibeepsync=polibeepsync.qtgui:main',
+        ],
+        'gui_scripts': [
+            'polibeepsync-gui=polibeepsync.qtgui:main'
+        ]
+    },
+    namespace_packages=['polibeepsync', 'polibeepsync.plugins']
 )
