92175d17ff6dafe376d82a3e60d054c522ec412c
Jacotsu <Jacotsu@users.noreply.github.com>
2018-12-14 05:38:39 +0100

    Updated requirements

--- setup.py
+++ setup.py
@@ -7,12 +7,14 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
+
 def read(*names, **kwargs):
     with open(
         os.path.join(os.path.dirname(__file__), *names),
         encoding=kwargs.get("encoding", "utf8")
     ) as fp:
         return fp.read()
+
 
 def find_version(*file_paths):
     version_file = read(*file_paths)
@@ -35,33 +37,34 @@ classifiers = [
 
 setup(
     name='poliBeePsync',
-    version = find_version("polibeepsync/__init__.py"),
-    url = "http://www.davideolianas.com/polibeepsync",
-    author= "Davide Olianas",
-    author_email= "ubuntupk@gmail.com",
-    license = 'GPLv3+',
-    install_requires = [
+    version=find_version("polibeepsync/__init__.py"),
+    url="http://www.davideolianas.com/polibeepsync",
+    author="Davide Olianas",
+    author_email="ubuntupk@gmail.com",
+    license='GPLv3+',
+    install_requires=[
         "requests",
         "appdirs",
         "PySide2",
         "beautifulsoup4",
+        "lxml",
         "pyparsing",
         "signalslot"
     ],
     python_requires='>=3.7',
-    packages = ['polibeepsync'],
-    description = "Sync files from https://beep.metid.polimi.it "
-                 "(for students of Politecnico di Milano)",
-    long_description = long_description,
-    classifiers = classifiers,
-    dependency_links = ['http://github.com/davethecipo/signalslot/tarball'
-                        '/py34#egg=signalslot-0.10.0'],
-    entry_points = {
-    'console_scripts': [
-        'polibeepsync=polibeepsync.qtgui:main',
-    ],
-    'gui_scripts': [
-        'polibeepsync-gui=polibeepsync.qtgui:main'
-    ]
-}
+    packages=['polibeepsync'],
+    description="Sync files from https://beep.metid.polimi.it "
+    "(for students of Politecnico di Milano)",
+    long_description=long_description,
+    classifiers=classifiers,
+    dependency_links=['http://github.com/davethecipo/signalslot/tarball'
+                      '/py34#egg=signalslot-0.10.0'],
+    entry_points={
+        'console_scripts': [
+            'polibeepsync=polibeepsync.qtgui:main',
+        ],
+        'gui_scripts': [
+            'polibeepsync-gui=polibeepsync.qtgui:main'
+        ]
+    }
 )
