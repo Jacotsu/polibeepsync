d2336cd251002d8dfcdc292d603a1588ad7232e1
Jacotsu <Jacotsu@users.noreply.github.com>
2021-03-24 13:18:52 +0100

    Fixed wrong dictionary key

--- polibeepsync/database_manager.py
+++ polibeepsync/database_manager.py
@@ -101,7 +101,7 @@ class DatabaseManager:
                 }
                 fil._file_dict = {**std_file_dict, **fil._file_dict}
                 fil._file_dict['extension'].replace('.', '')
-                fil._file_dict['name'].replace('UTF-8\'\'', '')
+                fil._file_dict['title'].replace('UTF-8\'\'', '')
 
             def update_folders_dicts(
                 folder: 'Folder',
